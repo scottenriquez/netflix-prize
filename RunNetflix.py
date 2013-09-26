ec8f6c733fc90c08fee77b12eeea033539ea281c
scottenriquez <lulz@utexas.edu>
2013-09-26 00:28:14 -0500

    Issue #21: hardcoded public repo paths

--- RunNetflix.py
+++ RunNetflix.py
@@ -12,16 +12,14 @@ from Netflix import netflix_read_actual, netflix_read_probe, netflix_compute_RMS
 # main
 # ----
 
-#ensure that all three caches are given
-assert sys.argv[1] != None and sys.argv[2] != None and sys.argv[3] != None
 #exact size of cache so resizing is not necessary
 #if not found in cache file for some reason, defaults to 2.5 rating
 userRatingCache = [2.5] * 2649430
 movieRatingCache = [2.5] * 17771
 #read caches
-netflix_make_cache(userRatingCache, open(sys.argv[1], "r"))
-netflix_make_cache(movieRatingCache, open(sys.argv[2], "r"))
-actualRatings = netflix_read_actual(open(sys.argv[3], "r"))
+netflix_make_cache(userRatingCache, open("/u/lara/cs373-netflix-tests/ghawk88-users_average_rating.txt", "r"))
+netflix_make_cache(movieRatingCache, open("/u/lara/cs373-netflix-tests/cmlinac-movies_average_rating.txt", "r"))
+actualRatings = netflix_read_actual(open("/u/lara/cs373-netflix-tests/ashorn49-bbell-user_and_rating.txt", "r"))
 #read probe from system in
 probeLines = netflix_read_probe(sys.stdin)
 #compute RMSE
